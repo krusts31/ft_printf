71b7f170394bfa5593aec96bad699a6699a418d3
krusts31 <krusts31@gmail.com>
2020-06-15 10:14:44 +0200

    s

--- ft_printf.c
+++ ft_printf.c
@@ -35,22 +35,120 @@ char    *ft_strdup(const char *s)
         return (ptr);
 }
 
+void    *ft_memset(void *s, int c, size_t n)
+{
+        size_t  i;
+
+        i = 0;
+        while (n > i)
+        {
+                ((char *)s)[i] = (unsigned char)c;
+                i++;
+        }
+        return (s);
+}
+
+void    *ft_memcpy(void *dest, const void *src, size_t n)
+{
+        char    *d; 
+        char    *s; 
+
+        d = (char *)dest;
+        s = (char *)src;
+        while (n > 0)
+        {
+                if (d == NULL && s == NULL)
+                        return (NULL);
+                *d = *s; 
+                d++;
+                s++;
+                n--;
+        }
+        if (d == NULL && s == NULL)
+                return (NULL);
+        return ((void *)dest);
+}
+
+char    *ft_memrcpy(char *dest, const char *src, size_t x)
+{
+        while (x > 0)
+        {
+                *dest = *src;
+                dest--;
+                src--;
+                x--;
+        }
+        return (dest);
+}
+
+void    *ft_memmove(void *dest, const void *src, size_t n)
+{
+        int                     x;
+        char            *desa;
+        const char      *gala;
+
+        x = n - 1;
+        desa = dest;
+        gala = src;
+        if (dest < src)
+                ft_memcpy(dest, src, n); 
+        else if (dest > src)
+        {
+                while (x > 0)
+                {
+                        desa++;
+                        gala++;
+                        x--;
+                }
+                ft_memrcpy(desa, gala, n); 
+        }
+        else if (dest == NULL || src == NULL)
+                return (NULL);
+        return ((void *)dest);
+}
+
+void    *ft_calloc(size_t nmemb, size_t size)
+{
+        void    *ptr;
+
+        ptr = malloc(nmemb * size);
+        if (ptr == 0)
+                return (NULL);
+        ptr = ft_memset(ptr, 0, nmemb * size);
+        return (ptr);
+}
+
+char	*ft_chop(const char	*s)
+{
+	int		x;
+	char	*conversion_specifier;
+
+	x = 0;
+	while (s[x] != '\0')
+	{
+		if (s[x] == 'c' || s[x] == 's' || s[x] == 'p'|| s[x] == 'd' || s[x] == 'i' || s[x] == 'u' || s[x] == 'x' || s[x] == 'X')
+		{
+			conversion_specifier = ft_calloc(x + 1, sizeof(char));
+			conversion_specifier = ft_memmove(conversion_specifier, s, x + 1);
+			return (conversion_specifier);
+		}
+		x++;
+	}
+	return (NULL);
+}
+
 char	*ft_find(const char	*s)
 {
+	char	*conversion_specifier;
+
 	if (s == NULL)
 		return (NULL);
-	printf("error");
 	while (*s != '\0')
 	{
-		printf("error");
 		if (*s == '%')
 		{
-			while (*s != 'c' || *s != 's' || *s != 'p'
-			|| *s != 'd' || *s != 'i' || *s != 'u'
-			|| *s != 'x' || *s != 'X' || *s != '%')
-				s++;
-			//we could increment a different varible hear and then copy that length and position 
-			return (ft_strdup(s));
+			conversion_specifier = ft_chop(s);
+			printf("%s\n", conversion_specifier);
 		}
 		s++;
 	}
@@ -59,20 +157,14 @@ char	*ft_find(const char	*s)
 
 int	ft_printf(const char *str, ...)
 {
-	char	*string;
-		printf("error");
-		string = ft_find(str);
-		printf("error");
-		printf("%s\n", string);
+		ft_find(str);
+		printf("%s\n", "Done");
 	return (0);
 }
 
 int     main()
 {
-	printf("Hello");
-	const char	*pptr;
-	pptr = "Hello World%d\nHello";
-	printf("main");
+	const char	pptr[] = "Hello World%010.5d\nHello%s.5\n%x       %0.10X        %lx    ";
         ft_printf(pptr);
         return(0);
 }
